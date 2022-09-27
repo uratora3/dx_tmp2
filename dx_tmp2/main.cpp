b04d8eabdab69ed1fc01300923bc3327159af1a8
fko2247003 <fko2247003@FIC-PCROOM3B-11>
2022-09-27 12:03:56 +0900

    プロジェクト ファイルを追加します。

---
+++ dx_tmp2/main.cpp
@@ -0,0 +1,51 @@
+#include "DxLib.h"
+
+namespace
+{
+	constexpr bool kWindowMode = true;
+	const char* const kTitleText = "ゲーム名";
+	constexpr int kScreenWidth = 1280;
+	constexpr int kScreenHeight = 720;
+	constexpr int kColorDepth = 32;
+}
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	// windowモードにする
+	ChangeWindowMode(kWindowMode);
+	SetMainWindowText(kTitleText);
+	SetGraphMode(kScreenWidth, kScreenHeight, kColorDepth);
+
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+	// ダブルバッファモード
+	SetDrawScreen(DX_SCREEN_BACK);
+
+
+	while (ProcessMessage() == 0)
+	{
+		LONGLONG  time = GetNowHiPerformanceCount();
+
+		ClearDrawScreen();
+
+		DrawString(160, 160, "a1aa2aaa3", GetColor(255, 255, 255));
+		
+		//裏画面を表画面を入れ替える
+		ScreenFlip();
+
+		// escキーを押したら終了する
+		if (CheckHitKey(KEY_INPUT_ESCAPE))	break;
+
+		// fpsを60に固定
+		while (GetNowHiPerformanceCount() - time < 16667)
+		{
+		}
+	}
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
